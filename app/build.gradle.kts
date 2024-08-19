1d78951a2b4c1335fc99d44c5e542cd7af90c343
立謙 <llc890410@gmail.com>
2024-08-19 16:20:58 +0800

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,16 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.okhttp)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
