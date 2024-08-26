c236e877368893befaba67ffd175c7ef94e1a1c6
立謙 <llc890410@gmail.com>
2024-08-26 16:51:32 +0800

    modify CryptoCurrency class

--- app/src/main/java/com/example/binanceticker/domain/model/CryptoCurrency.kt
+++ app/src/main/java/com/example/binanceticker/domain/model/CryptoCurrency.kt
@@ -1,12 +1,29 @@
 package com.example.binanceticker.domain.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CryptoCurrency(
-    val symbol: String,
-    val priceChange: String,
-    val priceChangePercent: String,
-    val lastPrice: String,
-    val quoteVolume: String,
+    @SerialName("symbol")               val symbol: String,
+    @SerialName("priceChange")          val priceChange: String,
+    @SerialName("priceChangePercent")   val priceChangePercent: String,
+    @SerialName("weightedAvgPrice")     val weightedAvgPrice: String,
+    @SerialName("prevClosePrice")       val prevClosePrice: String,
+    @SerialName("lastPrice")            val lastPrice: String,
+    @SerialName("lastQty")              val lastQty: String,
+    @SerialName("bidPrice")             val bidPrice: String,
+    @SerialName("bidQty")               val bidQty: String,
+    @SerialName("askPrice")             val askPrice: String,
+    @SerialName("askQty")               val askQty: String,
+    @SerialName("openPrice")            val openPrice: String,
+    @SerialName("highPrice")            val highPrice: String,
+    @SerialName("lowPrice")             val lowPrice: String,
+    @SerialName("volume")               val volume: String,
+    @SerialName("quoteVolume")          val quoteVolume: String,
+    @SerialName("openTime")             val openTime: Long,
+    @SerialName("closeTime")            val closeTime: Long,
+    @SerialName("firstId")               val firstId: Long,
+    @SerialName("lastId")               val lastId: Long,
+    @SerialName("count")                val count: Long
 )
