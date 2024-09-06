a9a3f58ec25ef8e15a346bc0f48665b9dfeda0d1
立謙 <llc890410@gmail.com>
2024-09-06 17:33:01 +0800

    fix enableEdgeToEdge, change background color

--- app/src/main/java/com/example/binanceticker/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/binanceticker/presentation/view/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.binanceticker.R
@@ -23,11 +25,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 獲取系統狀態欄和導航欄的高度 避免與狀態欄或導航欄重疊
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // 使用 WindowInsetsController 修改狀態欄文字顏色
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.isAppearanceLightStatusBars = false // false 表示淺色文字，true 表示深色文字
+
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
